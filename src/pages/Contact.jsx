ef7ae8de33a343174a774c6d2e32efc8cbc7a248
Brey Laude <laude822@gmail.com>
2023-09-26 17:17:08 +0800

    icon enhance

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,24 +8,19 @@ import "../styles/contact.css";
 
 const socialLinks = [
   {
-    url: "#",
+    url: "https://www.facebook.com/profile.php?id=61551613722978",
     icon: "ri-facebook-line",
   },
   {
-    url: "#",
-    icon: "ri-instagram-line",
-  },
-  {
-    url: "#",
-    icon: "ri-linkedin-line",
-  },
-  {
-    url: "#",
-    icon: "ri-twitter-line",
-  },
+    url: "",
+    icon: "ri-whatsapp-line",
+  }
 ];
 
 const Contact = () => {
+
+
+  
   return (
     <Helmet title="Contact">
       <CommonSection title="Contact" />
@@ -60,11 +55,11 @@ const Contact = () => {
               <div className="contact__info">
                 <h6 className="fw-bold">Contact Information</h6>
                 <p className="section__description mb-0">
-                  123 ZindaBazar, Sylhet, Bangladesh
+                  Brgy. San Miguel, Puerto Princesa City, Palawan 5300
                 </p>
                 <div className=" d-flex align-items-center gap-2">
                   <h6 className="fs-6 mb-0">Phone:</h6>
-                  <p className="section__description mb-0">+88683896366</p>
+                  <p className="section__description mb-0">(0936)-567-1353</p>
                 </div>
 
                 <div className=" d-flex align-items-center gap-2">
@@ -72,7 +67,7 @@ const Contact = () => {
                   <p className="section__description mb-0">example@gmail.com</p>
                 </div>
 
-                <h6 className="fw-bold mt-4">Follow Us</h6>
+                <h6 className="fw-bold mt-4">Or You Can Reach Us Via</h6>
 
                 <div className=" d-flex align-items-center gap-4 mt-3">
                   {socialLinks.map((item, index) => (
