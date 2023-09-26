97a97bb1b37637ea541d781f5ec6b2cc78fd8ded
Brey Laude <laude822@gmail.com>
2023-09-26 19:19:00 +0800

    form config + installed necessary dependencies

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Container, Row, Col, Form, FormGroup, Input } from "reactstrap";
 import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/CommonSection";
+import axios from 'axios';
 
 import "../styles/contact.css";
 
@@ -18,9 +19,43 @@ const socialLinks = [
 ];
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Make a POST request to your server's endpoint
+      await axios.post('/send-email', formData);
+
+      // Clear the form
+      setFormData({
+        name: "",
+        email: "",
+        message: "",
+      });
+
+      // Display a success message or handle redirection
+      alert('Message sent successfully');
+    } catch (error) {
+      console.error(error);
+      alert('An error occurred while sending the message');
+    }
+  };
 
 
-  
   return (
     <Helmet title="Contact">
       <CommonSection title="Contact" />
@@ -30,25 +65,39 @@ const Contact = () => {
             <Col lg="7" md="7">
               <h6 className="fw-bold mb-4">Get In Touch</h6>
 
-              <Form>
-                <FormGroup className="contact__form">
-                  <Input placeholder="Your Name" type="text" />
-                </FormGroup>
-                <FormGroup className="contact__form">
-                  <Input placeholder="Email" type="email" />
-                </FormGroup>
-                <FormGroup className="contact__form">
-                  <textarea
-                    rows="5"
-                    placeholder="Message"
-                    className="textarea"
-                  ></textarea>
-                </FormGroup>
-
-                <button className=" contact__btn" type="submit">
-                  Send Message
-                </button>
-              </Form>
+              <Form onSubmit={handleSubmit}>
+              <FormGroup className="contact__form">
+                <Input
+                  placeholder="Your Name"
+                  type="text"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleInputChange}
+                />
+              </FormGroup>
+              <FormGroup className="contact__form">
+                <Input
+                  placeholder="Email"
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                />
+              </FormGroup>
+              <FormGroup className="contact__form">
+                <textarea
+                  rows="5"
+                  placeholder="Message"
+                  className="textarea"
+                  name="message"
+                  value={formData.message}
+                  onChange={handleInputChange}
+                ></textarea>
+              </FormGroup>
+              <button className="contact__btn" type="submit">
+                Send Message
+              </button>
+            </Form>
             </Col>
 
             <Col lg="5" md="5">
